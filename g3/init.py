80e720d460d2daa4d563089d9c2edbe11ef101d3
Msdevusajeev <dsajeevkumar@actalentservices.com>
2026-02-09 16:58:06 +0530

    Update init.py

--- g3/init.py
+++ g3/init.py
@@ -1,1 +1,11 @@
+"""Checkpoint review tool package exports."""
+
+try:
+    # package import
+    from .logic import run
+except Exception:  # pragma: no cover
+    # script/folder import
+    from logic import run
+
+__all__ = ["run"]
 
